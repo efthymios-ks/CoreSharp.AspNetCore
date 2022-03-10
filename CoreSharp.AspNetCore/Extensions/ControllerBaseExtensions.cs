25d3accd8a134b6fdb886abc3eed72504b12373f
Efthymios Koktsidis <efthymios.koktsidis@onelity.com>
2022-03-10 10:10:06 +0200

    Extensions, Added `GetLinkedPage`1` method.

---
+++ CoreSharp.AspNetCore/Extensions/ControllerBaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using CoreSharp.Models.Pages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreSharp.AspNetCore.Extensions
+{
+    /// <summary>
+    /// <see cref="ControllerBase"/> extensions.
+    /// </summary>
+    internal static class ControllerBaseExtensions
+    {
+        /// <inheritdoc cref="HttpContextExtensions.GetLinkedPage{TEntity}(HttpContext, Page{TEntity})"/>
+        public static LinkedPage<TEntity> GetLinkedPage<TEntity>(this ControllerBase controller, Page<TEntity> page)
+            => controller?.HttpContext.GetLinkedPage(page);
+    }
+}
---
+++ CoreSharp.AspNetCore/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,42 @@
+﻿using CoreSharp.Models.Pages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using System;
+using static System.FormattableString;
+
+namespace CoreSharp.AspNetCore.Extensions
+{
+    /// <summary>
+    /// <see cref="HttpRequest"/> extensions.
+    /// </summary>
+    internal static class HttpRequestExtensions
+    {
+        /// <summary>
+        /// Build new <see cref="LinkedPage{TEntity}"/>
+        /// based on provided data.
+        /// </summary>
+        public static LinkedPage<TEntity> GetLinkedPage<TEntity>(this HttpRequest httpRequest, Page<TEntity> page)
+        {
+            _ = httpRequest ?? throw new ArgumentNullException(nameof(httpRequest));
+            _ = page ?? throw new ArgumentNullException(nameof(page));
+
+            if (httpRequest.Method != HttpMethods.Get)
+                throw new InvalidOperationException($"Pagination only works on {nameof(HttpMethods.Get)} methods (provided {nameof(httpRequest.Method)}).");
+
+            string BuildParameter(int pageNumber)
+            {
+                var queryBuilder = new QueryBuilder
+                {
+                    { nameof(PageOptions.PageNumber), Invariant($"{pageNumber}") },
+                    { nameof(PageOptions.PageSize), Invariant($"{page.PageSize}") }
+                };
+                return $"{httpRequest.Path}{queryBuilder}";
+            }
+
+            var previousPage = page.HasPrevious ? BuildParameter(page.PageNumber - 1) : null;
+            var nextPage = page.HasNext ? BuildParameter(page.PageNumber + 1) : null;
+
+            return new(page, previousPage, nextPage);
+        }
+    }
+}
